38d493e313e25267f71c43131a7945e684b39fea
aalarina <147263709+aalarina@users.noreply.github.com>
2026-09-26 13:01:06 +0300

    Add files via upload

--- src/data_loader.py
+++ src/data_loader.py
@@ -20,7 +20,7 @@ print("Cache enabled successfully!")
 
 
 def load_and_clean_data(year, grnd_prix_list):
-    """Retrieves race data, cleans it and return a ready-to-use DataFrame"""
+    """Retrieves race data, filters anomalies and return a clean ready-to-use DataFrame"""
     all_laps = []
 
     for gp in grnd_prix_list:
@@ -39,6 +39,8 @@ def load_and_clean_data(year, grnd_prix_list):
                 print(f"No lap data for {gp}, skip it")
                 continue
 
+            laps.loc[:, 'LapTime_Seconds'] = laps['LapTime'].dt.total_seconds()
+
             # Safely retrieve the track temperature
             try:
                 if hasattr(session, 'weather_data') and not session.weather_data.empty:
@@ -49,11 +51,27 @@ def load_and_clean_data(year, grnd_prix_list):
             except Exception:
                 track_temp = 30.0
 
-            laps['TrackTemp'] = track_temp
-            laps['GrandPrix'] = gp
+            laps.loc[:, 'TrackTemp'] = track_temp
+            laps.loc[:, 'GrandPrix'] = gp
 
-            all_laps.append(laps)
-            print(f"{gp} successfully processed ({len(laps)} laps loaded).")
+            # Filtration of anomalies (outliers)
+            # Find the best lap time for this race
+            best_lap_time = laps['LapTime_Seconds'].min()
+
+            # Find the median tempo of this race  
+            # Pitstop laps and accidences have anomalous lap times
+            typical_lap_time = laps['LapTime_Seconds'].median()
+
+            # Leave only laps that are slower then median + 5 seconds 
+            clean_laps = laps[laps['LapTime_Seconds'] <= (typical_lap_time + 5)].copy() 
+
+            # Create new variable  
+            clean_laps.loc[:, 'LapTime_Delta'] = clean_laps['LapTime_Seconds'] - best_lap_time
+
+            print(f"Anomalies removed: {len(laps) - len(clean_laps)} laps. Clean laps: {len(clean_laps)}")
+
+            all_laps.append(clean_laps)
+            print(f"{gp} successfully processed ({len(clean_laps)} laps loaded).")
 
         except Exception as e:
             print(f"Stage loading error {gp}: {e}")
@@ -65,21 +83,23 @@ def load_and_clean_data(year, grnd_prix_list):
     df = pd.concat(all_laps, ignore_index=True)
 
     # Filter the necessary columns and remove anomalies (yellow flags, safety car)
-    features= ['GrandPrix', 'Driver', 'LapNumber', 'LapTime', 'Compound', 'TyreLife', 'TrackTemp', 'TrackStatus']
+    features= ['GrandPrix', 'Driver', 'LapNumber', 'Compound', 'TyreLife', 'TrackTemp', 'LapTime_Seconds', 'LapTime_Delta']
     df = df[features].dropna()
-    df = df[df['TrackStatus'] == '1'] # Only clean racing laps
-
-    # Convert the time of a lap to seconds
-    df['LapTime_Seconds'] = df['LapTime'].dt.total_seconds()
-
-    return df.drop(columns=['LapTime', 'TrackStatus'])
+  
+    return df
 
 if __name__ == "__main__":
-    # Execute only if this file is run manually
     os.makedirs('./data', exist_ok=True)
     print("Start collecting data...")
-    tracks = ['Bahrain', 'Austria', 'Silverstone']
-    cleaned_df = load_and_clean_data(2024, tracks)
+
+    # tracks = ['Bahrain', 'Austria', 'Silverstone', 'Monza', 'Spa', 'Monaco', 'Barcelona', 'Singapore']
+    tracks_to_download = [
+        'Bahrain', 'Austria', 'Great Britain', 
+        'Italy', 'Belgium', 'Monaco', 
+        'Spain', 'Singapore'
+    ]
+
+    cleaned_df = load_and_clean_data(2024, tracks_to_download)
 
     cleaned_df.to_csv('./data/f1_clean_data_for_ml.csv', index=False)
     print("The clean data has been saved in './data/f1_clean_data_for_ml.csv'")
